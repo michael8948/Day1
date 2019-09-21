9242ebc7c7a844d20cf32ee6c0c2a975eed2cf0f
Michael8948 <2354690233@qq.com>
2019-09-21 11:43:47 +0800

    add else

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ var addNumber = function (x,y) {
     var total = x + y
     return total
 }
-var testAddNumbers = addNumber(4,8)
+var testAddNumbers = addNumber(4,3)
 // ===; <;>;<=;>=;!==
 if( testAddNumbers > 10 ) {
     console.log(testAddNumbers)
+} else {
+    console.log("The number is too small!")
 }
