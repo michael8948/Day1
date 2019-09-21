9e2ab7e1ab5baff328f090d3d74f07744a03c559
Michael8948 <2354690233@qq.com>
2019-09-21 11:50:11 +0800

    add &&

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ var addNumber = function (x,y) {
     var total = x + y
     return total
 }
-var testAddNumbers = addNumber(4,3)
+var testAddNumbers = addNumber(4,30)
 // ===; <;>;<=;>=;!==
-if( testAddNumbers > 10 ) {
+if (( testAddNumbers > 10 ) && (testAddNumbers < 20)) {
     console.log(testAddNumbers)
 } else {
     console.log("The number is too small!")
